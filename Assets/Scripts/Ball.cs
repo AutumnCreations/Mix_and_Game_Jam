3af37fa812817f2b0a440b687cb3a0b3e25fd482
Addikins <addikinsplays@gmail.com>
2020-11-13 15:29:13 -0700

    Add Ball script and auto-generated PlinkoBoard.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [Tooltip("Minimum force to add to the ball when colliding with a peg")]
+    [SerializeField] float randomOffsetMin = .01f;
+    [Tooltip("Maximum force to add to the ball when colliding with a peg")]
+    [SerializeField] float randomOffsetMax = .1f;
+
+    //Force to add to the ball when colliding with a peg
+    float xOffset;
+    Rigidbody2D ballBody;
+
+    Collider2D lastPeg;
+
+    void Start()
+    {
+        ballBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Peg"))
+        {
+            if (lastPeg == null || lastPeg != collision.collider)
+            {
+                xOffset = Random.Range(randomOffsetMin, randomOffsetMax);
+                //Returns xOffset randomly as positive or negative
+                xOffset *= Random.Range(0, 2) * 2 - 1;
+            }
+            ballBody.AddForce(new Vector2(xOffset, 0));
+
+            lastPeg = collision.collider;
+        }
+
+        print(xOffset);
+    }
+
+}
---
+++ Assets/Scripts/PlinkoBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlinkoBoard : MonoBehaviour
+{
+    [SerializeField] GameObject pegPrefab;
+    [SerializeField] Vector2 boardSize = new Vector2(4, 12);
+
+    float rowOffset;
+
+    void Start()
+    {
+        for (int y = 0; y < boardSize.y; y++)
+        {
+            if (y % 2 == 0) { rowOffset = .5f; }
+            else { rowOffset = 0; }
+            for (int x = 0; x < boardSize.x; x++)
+            {
+
+                Vector2 spawnPosition = new Vector2(transform.position.x - rowOffset + x, transform.position.y - y);
+                Instantiate(pegPrefab, spawnPosition, Quaternion.identity);
+                print(spawnPosition);
+            }
+        }
+    }
+}
